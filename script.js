af938dfbb2242e82ae17acd66da5aa0c6bf9d1e4
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-02-28 16:21:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+const person={
+    ism:null,
+    id:null,
+    parol:null
+}
+
+
+
+console.log(person);
+person.ism=prompt("Ismingizni kiriting!");
+
+
+console.log(person);
+
+
+person.id=prompt("Id kiriting");
+
+console.log(person);
+
+person.parol=prompt("Parol kiriting");
+
+console.log(person);
